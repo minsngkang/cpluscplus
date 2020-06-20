52f7aa464ae10abdbc59a1190570f9c8c41a8a1c
minsngkang <66898780+minsngkang@users.noreply.github.com>
2020-06-20 18:48:43 +0900

    Update 620_stack.cpp

--- 620_stack.cpp
+++ 620_stack.cpp
@@ -18,12 +18,12 @@ int main() {
         for (char ch : str) {
             if (ch == ' ' || ch == '\n') {
                 while (!s.empty()) {
-                    cout << s.top();
-                    s.pop();
+                    cout << s.top(); // // 에스탑 해서, 더블백 출력을
+                    s.pop(); // 얘는 그냥 지우는 역할만하는거군
                 }
                 cout << ch;
             } else {
-                s.push(ch);
+                s.push(ch); // 사실여기가 메인인데, 불쉿문에다가 넣었네 간단하게 
             }
         }
     }
